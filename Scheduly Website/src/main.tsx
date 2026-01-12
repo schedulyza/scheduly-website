09fb6f28067c7b112838cd2164f55779a96cf903
schedulyza <admin@scheduly.co.za>
2026-01-12 13:03:44 +0200

    Update main.tsx

--- Scheduly Website/src/main.tsx
+++ Scheduly Website/src/main.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 
-// If you have a global css file, keep this. If it errors, remove it.
-import "./app/styles/index.css";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
