9a6b8b1923e129d9679c6fe4ebe2bdbcd6a64e20
schedulyza <admin@scheduly.co.za>
2026-01-13 14:48:26 +0200

    Update routes.tsx

--- Scheduly Website/src/app/routes.tsx
+++ Scheduly Website/src/app/routes.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import { ContactPage } from "./components/ContactPage";
 import { PrivacyPolicyPage } from "./components/PrivacyPolicyPage";
@@ -8,13 +9,9 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-
-      {/* ContactPage currently expects onNavigate, so we pass a no-op for now */}
       <Route path="/contact" element={<ContactPage onNavigate={() => {}} />} />
-
       <Route path="/privacy" element={<PrivacyPolicyPage />} />
       <Route path="/terms" element={<TermsOfServicePage />} />
-
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
